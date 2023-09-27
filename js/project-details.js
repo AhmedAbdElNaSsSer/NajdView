d755dd5c5b596d74554d42fd71669a01ffc8bb79
AhmedAbdElNaSsSer <ahmedabdelnasser162@gmail.com>
2023-09-27 13:34:24 +0300

    سي

--- js/project-details.js
+++ js/project-details.js
@@ -7,7 +7,7 @@ const projects = [
         title:`مجمع مطاعم وكافيهات`,
         p:`  اسم المشروع / مجمع مطاعم وكافيهات
         <br>
-        الموقع / الباطن 
+        الموقع / حفر الباطن 
         `,
         folderName:'cloudSquare',
         imgs: ['001.jpg','002.jpg','003.jpg','004.jpg','005.jpg','006.jpg','007.jpg','008.jpg']
